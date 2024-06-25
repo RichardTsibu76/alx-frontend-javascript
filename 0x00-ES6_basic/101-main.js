28705c9390bb0a5d42b2917956769ba2eb62ecdf
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2024-06-25 01:24:49 +0100

    The last advance task

--- 0x00-ES6_basic/101-main.js
+++ 0x00-ES6_basic/101-main.js
@@ -1,3 +1,6 @@
+import createEmployeesObject from "./11-createEmployeesObject.js";
+import createReportObject from './12-createReportObject.js';
+import createIteratorObject from './100-createIteratorObject.js';
 import iterateThroughObject from './101-iterateThroughObject.js';
 
 
