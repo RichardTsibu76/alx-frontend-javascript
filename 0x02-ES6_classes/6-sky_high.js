314b44476292f70c57e396532a511cff5740451f
RICHARD TSIBU DARKOH <paakwesi5976@gmail.com>
2024-06-27 20:34:45 +0100

    task six updated

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -3,12 +3,19 @@ import Building from './5-building';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
+
+    if (typeof floors !== 'number') {
+      throw new TypeError('Floors must be a number');
+    }
     this._floors = floors;
   }
 
-  // floors getter
   get floors() {
     return this._floors;
+  }
+
+  get sqft() {
+    return this._sqft;
   }
 
   evacuationWarningMessage() {
