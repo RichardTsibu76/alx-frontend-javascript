fd0b829c1d165693b374906e5d9e659b462e74ad
RICHARD TSIBU DARKOH <paakwesi5976@gmail.com>
2024-06-27 20:18:22 +0100

    Task six

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -2,16 +2,27 @@ import Building from './5-building';
 
 class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
+    if (typeof floors !== 'number') {
+      throw new Error('Inavlid input');
+    }
     super(sqft);
     this._floors = floors;
   }
 
   get floors() {
-    return (this._floors);
+    return this._floors;
+  }
+
+  set floors(value) {
+    if (typeof value !== 'number') {
+      throw new Error('Floors must be a number');
+    } else {
+      this._floors = value;
+    }
   }
 
   evacuationWarningMessage() {
-    return (`Evacuate slowly the ${this.floors} floors`);
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
 
