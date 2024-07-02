a1c6d833802baa84f1432766a4eb90eb8d64ce01
RICHARD TSIBU DARKOH <paakwesi5976@gmail.com>
2024-07-03 00:44:27 +0100

    Task ten updated

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,11 +1,20 @@
-export default function updateUniqueItems(items) {
-  if (!(items instanceof Map)) {
+/**
+ * Map method updating to 1 pertinent to quantity
+ *
+ * @param {Map} map - the map method.
+ * @returns {Map} - An updated map with quantities
+ * @throws {Error} - Throws an error if not arg !map.
+ */
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
     throw new Error('Cannot process');
   }
-  for (const [key, value] of items) {
-    if (value === 1) {
-      items.set(key, 100);
+
+  map.forEach((quantity, item) => {
+    if (quantity === 1) {
+      map.set(item, 100);
     }
-  }
-  return items;
+  });
+
+  return map;
 }
